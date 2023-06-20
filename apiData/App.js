8a98c2ec96f60d1ca165c2f58d9226745dde04de
VISHAL-VADODARIYA <vadodariyavishal2@gmail.com>
2023-06-20 22:25:47 +0530

    change

--- apiData/App.js
+++ apiData/App.js
@@ -1,51 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/components/HomeScreen';
+import Favourite from './src/components/Favourite';
+import {NavigationContainer} from '@react-navigation/native';
+import { SafeAreaView, View } from 'react-native';
+const Tab = createBottomTabNavigator();
 
 function App() {
 
-  const [data, setData] = useState([]);
-  const getApiData = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    let result = await fetch(url).then(async (res) => {
-      if (res.status === 200) {
-        result = await res.json();
-        setData(result);
-        console.warn(data)
-      } else {
-        console.log("error while fetching")
-      }
-    }).catch((err) => {
-      console.log("err ::::", err);
-    });
-
-
-  }
-
-  useEffect(() => {
-    getApiData()
-  }, []);
 
   return (
-    <View>
-      <View style={{
-        backgroundColor: '#015F78'
-      }}>
-        <Text style={{ color: 'black', fontSize: 30,fontWeight:'bold', backgroundColor: '#fff', borderBottomWidth: 3, borderBottomColor: 'red', textAlign: 'center', paddingVertical: 10, borderRadius: 50, margin: 10 }}>Api Data Call</Text>
-      </View>
-      <ScrollView style={{ backgroundColor: '#fff'}}>
+    <View  style = {{flex : 1}}>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" options={{
+          tabBarLabel: 'Home',
+          
+        }} component={Home} />
+        <Tab.Screen name="Favourite" component={Favourite} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </View>
 
-        {data.length ?
-          data.map((item, index) => (
-            <View key={'item' + index} style={{ margin: 10, borderWidth: 1, borderBottomColor: 'orange', borderRadius: 10, overflow: 'hidden' }}>
-              <View style={{flex: 1, backgroundColor: '#00f5f6' }}>
-                <Text style={{ alignSelf: 'center', fontSize: 25,fontWeight:'bold',color:'#0a0990' }}>Id : {item.id}</Text>
-              </View>
-              <Text style={{ fontSize: 20, color: 'green', marginHorizontal: 10 }}>Title : {item.title}</Text>
-              <Text style={{ fontSize: 15, color: 'black', marginHorizontal: 10 }}>Body : {item.body}</Text>
-            </View>
-          ))
-          : null}
-      </ScrollView></View>
   );
 }
 
